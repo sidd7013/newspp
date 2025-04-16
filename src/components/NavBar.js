6c2067f90e52285e4b32fe8744aee9b6ac177bb6
sidd7013 <sinkarsiddhesh06@gmail.com>
2025-04-17 00:13:47 +0530

    navbar.js component added

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export class NavBar extends Component {
+ 
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavBar

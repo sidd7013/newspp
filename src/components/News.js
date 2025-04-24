a76417328baf7b73d122097ad8c42f08272c9ff5
sidd7013 <sinkarsiddhesh06@gmail.com>
2025-04-24 16:13:12 +0530

    Added prev and next button

--- src/components/News.js
+++ src/components/News.js
@@ -2,59 +2,77 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles = [
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+
 
   constructor() {
     super();
     console.log("Hello I am a constructor from News component");
     this.state = {
-      articles: this.articles,
-      loading: false//jab bhi koi cheese load ho rahi hogi toh make it true and show spinner and make it false when all data load   
+      articles: [],
+      loading: false,//jab bhi koi cheese load ho rahi hogi toh make it true and show spinner and make it false when all data load   
+      page:1
+    }
+  }
+
+  async componentDidMount() {
+    console.log("cmd");
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2573037d3ddc4215bd6e23dc261eb65c&page=1&pageSize=10";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults:parsedData.totalResults});
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2573037d3ddc4215bd6e23dc261eb65c&page=${this.state.page - 1}&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+
+  handleNextClick = async () => {
+    console.log("Next");
+    // Math.ceil means 4.6=>5,6.8=>7,etc Math.ceil(this.state.totalResults/10(totalResults))=number of pages
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/10)){
+
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2573037d3ddc4215bd6e23dc261eb65c&page=${this.state.page + 1}&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+    })
     }
   }
 
   render() {
-
+    console.log("render");
     return (
       <div className='container my-3'>
-        <h2>News Monkey -Top Headlines</h2>
+        <h1>News Monkey -Top Headlines</h1>
         {/* this.state.articles-are our articles,map-is higher order array method hain ,
         jab bhi aap .map() use karte ho toh har ek elements ko iterate karne ke liye aapko har ek element ko ek unique key deni padti hain*/}
-       
+
         <div className="row">
-        {this.state.articles.map((element)=>{
-          return  <div className="col-md-4" key={element.url}>
-      <NewsItem  title={element.title.slice(0,48)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url} />
-        </div>
-        })}
-         
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+            </div>
+          })}
+          <div className="d-flex justify-content-between">
+            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+
+          </div>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />  
+          <img src={imageUrl?imageUrl:"https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2211327891.jpg?c=16x9&q=w_800,c_fill" } className="card-img-top" alt="..." />  
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
       </div>

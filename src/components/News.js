3d3b4c7f00af66e69c70310faf963cecb7af4d50
sidd7013 <sinkarsiddhesh06@gmail.com>
2025-04-28 10:48:56 +0530

    Added react-top-loading-bar and fixed position of badge

--- src/components/News.js
+++ src/components/News.js
@@ -37,16 +37,20 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?&country=${this.props.country}&category=${this.props.category}&apiKey=2573037d3ddc4215bd6e23dc261eb65c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
